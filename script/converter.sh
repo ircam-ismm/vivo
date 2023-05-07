26a8f33b39968285447dd55338fb6a45f4a78a5a
Diemo Schwarz <schwarz@ircam.fr>
2023-05-07 15:39:08 +0200

    give dir as argument

--- script/converter.sh
+++ script/converter.sh
@@ -1,16 +1,20 @@
 #! /bin/bash
+
+dir=${1:-.}
+
 echo
-echo "All .MOV files in this directory will be converted to hap codec. Original files will be kept"
+echo "All .MOV files in directory $dir will be converted to hap codec. Original files will be kept"
 echo
 read -p "Are you sure ? (y/n) " -n 1 -r
 echo
 
+
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 
-	for file in *.MOV
+	for file in $dir/*.mov $dir/*.MOV
 	do
-		ffmpeg -i "$file" -c:v hap "$file"_hap.MOV
+		ffmpeg -i "$file" -c:v hap "$file"_hap.mov
 		echo "done $file"
 	done
 	echo
